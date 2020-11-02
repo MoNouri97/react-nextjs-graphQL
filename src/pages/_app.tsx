794f58de3d96ddde2c87361aa6feff75edf7bdc8
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-11-02 17:57:23 +0100

    urql cache update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,43 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import { cacheExchange } from '@urql/exchange-graphcache';
 import { AppProps } from 'next/app';
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { LoginMutation, MeDocument, MeQuery } from '../generated/graphql';
 
 import theme from '../theme';
+import { betterUpdateQuery } from '../utils/betterUpdateQuery';
 
 const client = createClient({
 	url: 'http://localhost:4000/graphql',
 	fetchOptions: {
 		credentials: 'include',
 	},
+	exchanges: [
+		dedupExchange,
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (_result, args, cache, info) => {
+						betterUpdateQuery<LoginMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							_result,
+							(loginResult, queryData) => {
+								if (loginResult.login.errors) {
+									return queryData;
+								} else {
+									return {
+										me: loginResult.login.user,
+									};
+								}
+							},
+						);
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
 });
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
